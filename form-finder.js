982496123c36dd7ac1a9e17bb31096195f7481dd
Karl Massieux <icefreezer86@gmail.com>
2022-06-29 17:11:54 +0200

    Update form-finder.js

--- form-finder.js
+++ form-finder.js
@@ -7,30 +7,31 @@ while(document.querySelector(patternF)) {
 
 //Search a form, if there is not, use document as the form
 //Rechercher un formulaire, si pas de formulaire, considérer toute la page comme étant le formulaire
-formulaire = document.querySelector('form') ? document.querySelector('form') : document;
-form_id = formulaire.id;
+loginform = document.querySelector('form') ? document.querySelector('form') : document;
+form_id = loginform.id;
 
 //almost all the time, the password-input has type=password
 //99% du temps le champs du mot de passe est de type password (permet d'avoir les *****)
-pass_id = formulaire.querySelector('input[type=password]').id;
+pass_input = loginform.querySelector('input[type=password]')
+pass_id = pass_input ? pass_input.id : "";
 
 //remove all the hidden inputs
 //supprimer tous les input cachés
 patternH = `input[type=hidden]`
-while(formulaire.querySelector(patternH)) {
-    formulaire.querySelector(patternH).remove();
+while(loginform.querySelector(patternH)) {
+    loginform.querySelector(patternH).remove();
 }
 
 //search pattern for the user-input
 //Motif de recherche pour le champs d'identification (user)
 patternU = `input[id*=user],input[name*=user],
-            input[id*=User],input[name*=User],
-            input[id*=login],input[name*=login],
-            input[id*=Login],input[name*=Login],
-            input[id*=mail],input[name*=mail],
-            input[id*=dentifier],input[name*=dentifier]`;
+        input[id*=login],input[name*=login],
+        input[id*=Login],input[name*=Login],
+        input[id*=mail],input[name*=mail],
+        input[id*=dentifier],input[name*=dentifier]`;
 
-user_id = formulaire.querySelector(patternU).id;
+user_input = loginform.querySelector(patternU)
+user_id = user_input ? user_input.id : "";
 
 //return the result as id's separated by '|' for later treatment
 //renvoyer le résultat sous la forme d'une chaine qu'il ne reste plus qu'à traiter
